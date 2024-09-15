f46aa6f5ca534f502c73888290fe9aec6c963a3a
Killian Tornese <killian.tornese@gmail.com>
2024-09-15 23:40:01 +0200

    fix(cli): update import paths to include file extensions

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,8 @@
 import { version } from '../package.json';
 import { cli } from 'cleye';
-import config from './commands/config';
-import init from './commands/init';
-import GitPilot from './core/gitpilot';
+import config from './commands/config.js';
+import init from './commands/init.js';
+import GitPilot from './core/gitpilot.js';
 
 const gitPilot = new GitPilot();
 
@@ -20,7 +20,6 @@ cli(
 		ignoreArgv: (type: any) => type === 'unknown-flag' || type === 'argument',
 	},
 	(argv: any) => {
-		console.log('Unknown command:');
 		gitPilot.generate();
 	}
 );
