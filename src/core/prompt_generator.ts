ee3f00664de739c3907031f1c52c113da6f57d1e
Killian Tornese <killian.tornese@gmail.com>
2024-09-27 17:04:47 +0200

    feat(prompt_generator): update message for commit guidelines

--- src/core/prompt_generator.ts
+++ src/core/prompt_generator.ts
@@ -22,6 +22,7 @@ Generate a concise git commit message for the following code diff with the given
 				return `Write a commit message that sounds like a human wrote it.`;
 			case 'conventional':
 				return `Write a commit message that follows the conventional commit message format.
+Only the first line will be used as the commit message.
 Here are the concise rules : The keywords “MUST”, “MUST NOT”, “SHALL”, etc., are defined as per RFC 2119.
 Commits MUST start with a type (e.g., feat, fix) followed by an OPTIONAL scope, an OPTIONAL ! for breaking changes, and a REQUIRED colon and space. feat is for new features. fix is for bug fixes. Other types MAY be used (e.g., docs). A scope is an OPTIONAL noun in parentheses (e.g., fix(parser)). The description MUST follow immediately after the type/scope prefix and briefly summarize the change. An OPTIONAL body starts one blank line after the description and MAY have any number of paragraphs. OPTIONAL footers start one blank line after the body and MUST use tokens with a : or # separator, and - instead of spaces, except for BREAKING CHANGE, which MAY also be used. Breaking changes MUST be indicated by a ! in the prefix or as a footer with BREAKING CHANGE: and a description. Commits are case-insensitive, except for BREAKING CHANGE.`;
 			case 'imitate':
